f64daf424462416892c4010d823a22b363f9ccde
Richard Acosta <timber3252@outlook.com>
2023-11-30 03:04:57 +0800

    feat: migrate to timber-laptop

--- dotfiles/config/fish/functions/tmp/vmls.fish
+++
@@ -1,9 +0,0 @@
-function nsls --description 'list nspawn containers'
-  echo "Running containers:"
-  sudo machinectl list $argv;
-
-  echo
-
-  echo "Available containers:"
-  sudo machinectl list-images $argv;
-end
